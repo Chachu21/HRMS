1eac00cb8ca78939900976c2e3fd494a3b67c949
BDU120314 <tsecha6@gmail.com>
2023-05-25 03:07:18 +0300

    first commit

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+const app = express();
+const port = process.env.PORT || 5002;
+
+//route
+app.get("/", 
+(req, res) =>{
+res.send("welcome")
+  });
+
+
+  app.listen(port, 
+  () =>{
+  console.log("server is running")
+    })

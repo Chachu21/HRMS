9702b707c8ae3d391c9ae768f45d62ea3013f4c4
MulukenDemis <amsaledemismuller@gmail.com>
2023-05-28 14:42:39 +0300

    contact added

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -60,7 +60,7 @@ const Login = () => {
         } else {
           localStorage.removeItem("rememberMe"); // Remove Remember Me value from local storage
         }
-        navigate("/applicant");
+        navigate("/admin");
       }
     } catch (error) {
       dispatch(setError(error.message));
--- frontend/src/pages/Vacancy.jsx
+++ frontend/src/pages/Vacancy.jsx
@@ -15,12 +15,12 @@ const Vacancy = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center gap-10 py-5">
+    <div className="flex flex-col items-center justify-center gap-10 py-5 h-full">
       <div className="flex items-center rounded-[5px]">
         <form
           action=""
           onSubmit={handleSubmit}
-          className="flex items-center justify-center"
+          className="flex items-center justify-center fixed mt-8 ml-auto"
         >
           <input
             id="search"
@@ -30,18 +30,20 @@ const Vacancy = () => {
               setFormData({ ...formData, searchQuery: e.target.value });
             }}
             type="text"
-            className="bg-gray-100 w-[250px] outline-none border-2 border-gray-300 pl-3 lg:w-[350px] h-10 rounded-[5px] placeholder:text-[18px] leading-4 font-normal"
+            className="bg-gray-100 w- outline-none border-2 border-gray-300 pl-3 lg:w-[350px] h-12 rounded-2xl placeholder:text-[18px] leading-4 font-normal "
             placeholder="search here...."
           />
           <button
             type="submit"
-            className="bg-blue-400 h-10 flex px-[14px] justify-center items-center rounded-tr-[5px] rounded-br-[5px] cursor-pointer"
+            className="bg-blue-400 h-12 flex px-[14px] justify-center items-center rounded-tr-[5px] rounded-br-[5px] cursor-pointer"
           >
             <FaSearch color="white" />
           </button>
         </form>
-      </div>
-      <div className="grid lg:grid-cols-3 gap-5 md:grid-cols-2 bg-gray-200 p-5">
+       </div>
+       <div className="grid lg:grid-cols-3 gap-5 md:grid-cols-2 bg-gray-200 p-5  max-h-96 w-full ">
+
+        
         <VacancyCard />
         <VacancyCard />
         <VacancyCard />

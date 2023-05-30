fc04799fe646d9f8df437c2988f51f59f0b4d710
MulukenDemis <amsaledemismuller@gmail.com>
2023-05-30 17:50:09 +0300

    employee and deptHead dashboard updated

--- backend/models/leave_request/leave_request.js
+++ backend/models/leave_request/leave_request.js
@@ -1,34 +1,42 @@
 const Sequelize = require('sequelize');
 module.exports = function(sequelize, DataTypes) {
-  return sequelize.define('leave_request', {
-    staff_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'staff',
-        key: 'id'
-      }
-    },
-    reason: {
-      type: DataTypes.TEXT,
-      allowNull: false
-    },
-    clearance: {
-      type: DataTypes.BLOB,
-      allowNull: false
-    }
-  }, {
-    sequelize,
-    tableName: 'leave_request',
-    timestamps: false,
-    indexes: [
-      {
-        name: "staff_id",
-        using: "BTREE",
-        fields: [
-          { name: "staff_id" },
-        ]
+  return sequelize.define(
+    "leave_request",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey:true,
+        autoIncrement:true
       },
-    ]
-  });
+      staff_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: "staff",
+          key: "id",
+        },
+      },
+      reason: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      clearance: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      tableName: "leave_request",
+      timestamps: false,
+      indexes: [
+        {
+          name: "staff_id",
+          using: "BTREE",
+          fields: [{ name: "staff_id" }],
+        },
+      ],
+    }
+  );
 };
--- frontend/src/comopnents/dashboard/departmentHead/ManageJobRank.jsx
+++ frontend/src/comopnents/dashboard/departmentHead/ManageJobRank.jsx
@@ -8,7 +8,7 @@ const ManageJobRank = () => {
 
   useEffect(() => {
     axios
-      .get("http://localhost:5002/job_rank")
+      .get("http://localhost:5002/api/v1/jobRank")
       .then((response) => {
         setJobRankData(response.data);
       })
@@ -22,7 +22,7 @@ const ManageJobRank = () => {
   //   };
 
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col ml-[20%] mr-[1%]">
       <h1 className="text-2xl font-bold mb-4">Job Rank Table</h1>
       <table className="table-auto border-collapse border border-gray-400">
         <thead>
@@ -39,6 +39,10 @@ const ManageJobRank = () => {
               <td className="px-4 py-2">{rank.staff_id}</td>
               <td className="px-4 py-2">{rank.level}</td>
               <td className="px-4 py-2">{rank.cv}</td>
+              {/* <td className="px-4 py-2">
+                <img src={rank.cv} alt="Image" />
+              </td> */}
+
               <td className="px-4 py-2">
                 <button
                   className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
